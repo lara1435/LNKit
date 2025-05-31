78130560f181d6ca246ea916bfb855da0e73619a
Lakshmi Narayanan N <lara1435@gmail.com>
2025-05-31 14:31:42 +0530

    Second commit

--- Package.swift
+++ Package.swift
@@ -5,11 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "LNKit",
+    platforms: [
+        .iOS(.v18)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "LNKit",
-            targets: ["LNKit"]),
+        .library(name: "LNKit", targets: ["LNKit"]),
+        .library(name: "LNKit2", targets: ["LNKit"]),
+        .library(name: "LNKit3", targets: ["LNKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
