f6e81d6a927e492fd3ec00c62232a53cfb4604bf
Lakshmi Narayanan N <lara1435@gmail.com>
2025-05-31 14:23:23 +0530

    Initial commit

---
+++ Sources/LNKit/Employee.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  LNKit
+//
+//  Created by Lakshmi Narayanan N on 31/05/25.
+//
+
+import Foundation
---
+++ Tests/LNKitTests/EmployeeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  LNKit
+//
+//  Created by Lakshmi Narayanan N on 31/05/25.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
